25c259a28c3a542d0d25e900687e9f3160686bfe
Your Name <you@example.com>
2021-11-07 11:20:21 +0000

    dev

---
+++ ArchiveStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Microsoft.Data.Sqlite;
+using Neo.IO;
+using Neo.Ledger;
+using Neo.Network.P2P.Payloads;
+using Neo.Persistence;
+using Neo.SmartContract;
+
+namespace Neo.Plugins
+{
+    public class ArchiveStore : Plugin, IPersistencePlugin
+    {
+        public void OnPersist(NeoSystem system, Block block, DataCache snapshot, IReadOnlyList<Blockchain.ApplicationExecuted> applicationExecutedList) => snapshot.GetChangeSet().ToList().ForEach(v => snapshot.Add(new StorageKey() { Id = int.MinValue, Key = v.Key.ToArray().Concat(BitConverter.IsLittleEndian ? BitConverter.GetBytes(block.Index).Reverse() : BitConverter.GetBytes(block.Index)).ToArray() }, new StorageItem(v.Item.ToArray().Append((byte)v.State).ToArray())));
+    }
+}
