e589854470147e9d84f365e8130dab790d5ed767
Your Name <you@example.com>
2021-11-07 08:47:01 +0000

    dev

--- ArchiveStore.cs
+++ ArchiveStore.cs
@@ -5,12 +5,14 @@ using Neo.IO;
 using Neo.Ledger;
 using Neo.Network.P2P.Payloads;
 using Neo.Persistence;
-using Neo.SmartContract;
 
 namespace Neo.Plugins
 {
     public class ArchiveStore : Plugin, IPersistencePlugin
     {
-        public void OnPersist(NeoSystem system, Block block, DataCache snapshot, IReadOnlyList<Blockchain.ApplicationExecuted> applicationExecutedList) => snapshot.GetChangeSet().ToList().ForEach(v => snapshot.Add(new StorageKey() { Id = int.MinValue, Key = v.Key.ToArray().Concat(BitConverter.IsLittleEndian ? BitConverter.GetBytes(block.Index).Reverse() : BitConverter.GetBytes(block.Index)).ToArray() }, new StorageItem(v.Item.ToArray().Append((byte)v.State).ToArray())));
+        IStore store;
+        public override void Dispose() => store.Dispose();
+        protected override void OnSystemLoaded(NeoSystem system) => store = system.LoadStore(String.Format("Data_Archive_{0}", system.Settings.Network.ToString("X")));
+        public void OnPersist(NeoSystem system, Block block, DataCache snapshot, IReadOnlyList<Blockchain.ApplicationExecuted> applicationExecutedList) => snapshot.GetChangeSet().ToList().ForEach(v => store.Put(v.Key.ToArray().Concat(BitConverter.IsLittleEndian ? BitConverter.GetBytes(block.Index).Reverse() : BitConverter.GetBytes(block.Index)).ToArray(), v.Item.ToArray().Append((byte)v.State).ToArray()));
     }
 }
